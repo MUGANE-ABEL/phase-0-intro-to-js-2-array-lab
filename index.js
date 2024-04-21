c34d2f3c8da38e7ff17f5dcecb4362433435c1eb
Mugane Abel <muganeabel@gmail.com>
2024-04-21 22:23:34 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.push ("Ralph")
+}
+
+const destructivelyPrependCat= function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.unshift("Bob")
+}
+
+const destructivelyRemoveLastCat= function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat= function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.shift()
+}
+
+const appendCat= [...cats, "Broom"]
+
+
+const prependCat= ["Arnold", ...cats]
+
+const removeLastCat= function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.slice(0, cats.length-1)
+}
+
+const removeFirstCat= function(){
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield')
+    cats.slice(1)
+}
